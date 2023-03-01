6d261a2328f53c3b70d7b04b9f335f3b496d2410
Your Name <you@example.com>
2023-03-01 13:33:03 -0600

    Match the latest Docker version

--- deploy_stat_collector.sh
+++ deploy_stat_collector.sh
@@ -6,7 +6,7 @@ docker run -d \
 --network host \
 --volume $(pwd):/data \
 --entrypoint python3 \
-classicblue/ptychonn:0.5.4 \
+classicblue/ptychonn:0.5.5 \
   /app/stat-exporter.py \
   --output-filepath "/data/output/stat.txt" \
   --channel-name "processor:1:status" \
